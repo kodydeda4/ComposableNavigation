97dc406b8a3a9a1e9c37c88f90e9e215d4564bef
Kody Deda <kodydeda4@gmail.com>
2023-02-02 12:41:54 -0500

    Initial Commit

---
+++ ComposableNavigation/ComposableNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableNavigationApp.swift
+//  ComposableNavigation
+//
+//  Created by Kody Deda on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ComposableNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
